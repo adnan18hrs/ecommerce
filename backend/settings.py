a4d77d8de8654f5c5c1288bbf90b7d2389278595
adnan <khan.adnan346@gmail.com>
2021-12-26 15:36:18 +0530

    ..

--- backend/settings.py
+++ backend/settings.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4hk^7&z^2hwurnm0r9^qfmgl)hg+oh@!k8!z(w%59qg1@c(fmt"
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "adnan-e-commerce.herokuapp.com"]
 INSTALLED_APPS = [
     "django.contrib.admin",
